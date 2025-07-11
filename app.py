08874bd82eae89918529ac678ffc79e1bcc6c050
uimin115 <g76567656g@gmail.com>
2025-07-11 16:43:12 +0900

    fix: bind app to 0.0.0.0 and use PORT env

--- app.py
+++ app.py
@@ -43,4 +43,5 @@ def download(filename):
     return send_file(filepath, as_attachment=True)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=True)
